ec86ec189330429f252d07186e6a4e7c2ecfff0b
Elliot Huffman <ehuffman@elliot-labs.com>
2017-02-02 15:11:47 -0500

    Add GUI for categories page. Also swapped settings page and content page content.

--- mobile/src/main/java/com/elliot_labs/timetracker/SettingsFragment.java
+++ mobile/src/main/java/com/elliot_labs/timetracker/SettingsFragment.java
@@ -1,17 +1,23 @@
 package com.elliot_labs.timetracker;
 
 
+import android.app.NotificationManager;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.NotificationCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class SettingsFragment extends Fragment {
+public class SettingsFragment extends Fragment implements OnClickListener {
 
 
     public SettingsFragment() {
@@ -22,8 +28,39 @@ public class SettingsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false);
-    }
 
+        View v = inflater.inflate(R.layout.fragment_settings, container, false);
+
+        Button toastButton = (Button) v.findViewById(R.id.toastButton);
+        Button notificationButton = (Button) v.findViewById(R.id.notificationButton);
+
+        toastButton.setOnClickListener(this);
+        notificationButton.setOnClickListener(this);
+        // Inflate the layout for this fragment
+        return v;
+    }
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.toastButton:
+                Context contextToast = getActivity().getApplicationContext();
+                CharSequence text = "Test toast...";
+                int duration = Toast.LENGTH_SHORT;
+                Toast.makeText(contextToast, text, duration).show();
+                break;
+
+            case R.id.notificationButton:
+                Context contextNotification = getActivity().getApplicationContext();
+                NotificationCompat.Builder mBuilder = (android.support.v7.app.NotificationCompat.Builder)
+                        new NotificationCompat.Builder(contextNotification)
+                                .setSmallIcon(R.mipmap.ic_launcher)
+                                .setContentTitle("My notification")
+                                .setContentText("Hello World!");
+                NotificationManager mNotificationManager =
+                        (NotificationManager) getActivity().getSystemService(Context.NOTIFICATION_SERVICE);
+                mNotificationManager.notify(0, mBuilder.build());
+
+                break;
+        }
+    }
 }
