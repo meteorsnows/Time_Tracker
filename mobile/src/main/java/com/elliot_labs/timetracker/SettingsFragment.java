9660e1128414a8ccff6a08fdf062bf80c1a8998d
Elliot Huffman <ehuffman@elliot-labs.com>
2017-02-05 14:53:03 -0500

    Optimize imports.

--- mobile/src/main/java/com/elliot_labs/timetracker/SettingsFragment.java
+++ mobile/src/main/java/com/elliot_labs/timetracker/SettingsFragment.java
@@ -8,8 +8,8 @@ import android.support.v4.app.Fragment;
 import android.support.v7.app.NotificationCompat;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.view.View.OnClickListener;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Toast;
 
