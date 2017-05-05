a0b400f83b1954568d70eb363f8d5906494a436c
Elliot Huffman <ehuffman@elliot-labs.com>
2017-05-04 21:22:01 -0400

    Redesigned the database for the ability to allow time tracking while device is powered off. Also cleaned up names and variables.

--- mobile/src/main/java/com/elliot_labs/timetracker/DatabaseHelper.java
+++ mobile/src/main/java/com/elliot_labs/timetracker/DatabaseHelper.java
@@ -16,21 +16,26 @@ class DatabaseHelper extends SQLiteOpenHelper {
 
     private static final String DATABASE_NAME = "Times.db";
 
-    private static final String COL_ID = "ID INTEGER PRIMARY KEY AUTOINCREMENT";
+    private static final String COL_GLOBAL_ID = "ID INTEGER PRIMARY KEY AUTOINCREMENT";
 
     private static final String TABLE_SAVED = "saved_times";
-    private static final String COL_DATE = "initial_date INTEGER";
-    private static final String COL_TOTAL = "total_time INTEGER";
-    private static final String COL_CATEGORY = "category INTEGER";
+    private static final String COL_SAVED_DATE = "initial_date INTEGER";
+    private static final String COL_SAVED_TOTAL = "total_time INTEGER";
+    private static final String COL_SAVED_CATEGORY = "category INTEGER";
 
     private static final String TABLE_CATEGORIES = "categories";
-    private static final String COL_NAME = "name TEXT";
-    private static final String COL_PARENT = "parent INTEGER";
+    private static final String COL_CATEGORIES_NAME = "name TEXT";
+    private static final String COL_CATEGORIES_PARENT = "parent INTEGER";
 
-    private static final String TABLE_WORKING = "in_progress";
-    private static final String COL_START = "start_time INTEGER";
-    private static final String COL_CURRENT_TOTAL = "current_total INTEGER";
-    private static final String COL_WORKING_CATEGORY = "category TEXT";
+    private static final String TABLE_SETTINGS = "settings";
+    private static final String COL_SETTINGS_NAME = "name TEXT";
+    private static final String COL_SETTINGS_VALUE = "value TEXT";
+
+    private static final String TABLE_TIMING = "currently_timing";
+    private static final String COL_TIMING_CREATED = "date_created INTEGER";
+    private static final String COL_TIMING_TIMING = "timing_from INTEGER";
+    private static final String COL_TIMING_TOTAL = "current_total INTEGER";
+    private static final String COL_TIMING_CATEGORY = "category INTEGER";
 
 
 
@@ -40,20 +45,25 @@ class DatabaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("create table " + TABLE_CATEGORIES + " ("
-                + COL_ID + ","
-                + COL_NAME + ","
-                + COL_PARENT + ")");
         db.execSQL("create table " + TABLE_SAVED + " ("
-                + COL_ID + ","
-                + COL_DATE + ","
-                + COL_TOTAL + ","
-                + COL_CATEGORY + ")");
-        db.execSQL("create table " + TABLE_WORKING + " ("
-                + COL_ID + ","
-                + COL_START + ","
-                + COL_CURRENT_TOTAL + ","
-                + COL_WORKING_CATEGORY + ")");
+                + COL_GLOBAL_ID + ","
+                + COL_SAVED_DATE + ","
+                + COL_SAVED_TOTAL + ","
+                + COL_SAVED_CATEGORY + ")");
+        db.execSQL("create table " + TABLE_CATEGORIES + " ("
+                + COL_GLOBAL_ID + ","
+                + COL_CATEGORIES_NAME + ","
+                + COL_CATEGORIES_PARENT + ")");
+        db.execSQL("create table " + TABLE_SETTINGS + " ("
+                + COL_GLOBAL_ID + ","
+                + COL_SETTINGS_NAME + ","
+                + COL_SETTINGS_VALUE + ")");
+        db.execSQL("create table " + TABLE_TIMING + " ("
+                + COL_GLOBAL_ID + ","
+                + COL_TIMING_CREATED + ","
+                + COL_TIMING_TIMING + ","
+                + COL_TIMING_TOTAL + ","
+                + COL_TIMING_CATEGORY + ")");
 
         // Creates teh tables and columns in the DB for data storage
     }
@@ -67,12 +77,12 @@ class DatabaseHelper extends SQLiteOpenHelper {
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
 
-        contentValues.put(COL_NAME.split(" ", 0)[0], name);
+        contentValues.put(COL_CATEGORIES_NAME.split(" ", 0)[0], name);
         if (parent != null){
             if (parent > 0) {
-                contentValues.put(COL_PARENT.split(" ", 0)[0], parent);
+                contentValues.put(COL_CATEGORIES_PARENT.split(" ", 0)[0], parent);
             } else {
-                contentValues.put(COL_PARENT.split(" ", 0)[0], 0);
+                contentValues.put(COL_CATEGORIES_PARENT.split(" ", 0)[0], 0);
             }
         }
 
