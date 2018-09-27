14296b291ac18fa6dae6556817454a88bfe17ea7
Elliot Huffman <thelich2@gmail.com>
2018-09-27 16:52:03 -0400

    Add Windows App
    Add the initial UWP standalone app.

---
+++ UWP Native/Time Tracker/Services/FirstRunDisplayService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using Microsoft.Toolkit.Uwp.Helpers;
+
+using Time_Tracker.Views;
+
+namespace Time_Tracker.Services
+{
+    public static class FirstRunDisplayService
+    {
+        private static bool shown = false;
+
+        internal static async Task ShowIfAppropriateAsync()
+        {
+            if (SystemInformation.IsFirstRun && !shown)
+            {
+                shown = true;
+                var dialog = new FirstRunDialog();
+                await dialog.ShowAsync();
+            }
+        }
+    }
+}
