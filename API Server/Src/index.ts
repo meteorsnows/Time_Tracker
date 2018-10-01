371cb8ee68963ebb5231f0498c45299dc43cc534
Elliot Huffman <thelich2@gmail.com>
2018-10-01 13:07:48 -0400

    Update example
    Update hello world example to use the express web server.

--- API Server/Src/index.ts
+++ API Server/Src/index.ts
@@ -1,1 +1,15 @@
-console.log("Hello world!");
+import express from "express";
+
+// Initialize express.
+const app = express();
+
+// Define variables
+const port = 8080;
+
+app.get("/", (req, res) => {
+    res.send("Hello world!");
+});
+
+app.listen(port, () => {
+    console.log("server running.");
+});
